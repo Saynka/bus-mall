76632cb2909a4d2a5afdc738cb5cb6af4ec13abb
kale lesko <sankakale@gmail.com>
2020-08-17 19:10:31 -0700

    day1

--- js/app.js
+++ js/app.js
@@ -5,8 +5,8 @@ var parentElement = document.getElementById('picture-array');
 
 var picArray = [];
 
-function Pic(path, alt, title) {
-  this.filepath = path;
+function Pic(filepath, alt, title) {
+  this.filepath = `../img/${name}.${ext}`;
   this.alt = alt;
   this.title = title;
   this.click = 0;
@@ -14,7 +14,42 @@ function Pic(path, alt, title) {
   picArray.push(this);
 }
 
+new Pic('bag', 'jpg');
+new Pic('banana', 'jpg');
+new Pic('bathroom', 'jpg');
 
 
+function getRandomImage(){
+  var randomIndex = getRandomNumber(picArray.length);
+  var chosenImage = picArray[randomIndex];
+  var imageElement = document.createElement('img');
+  imageElement.setAttribute('src', chosenImage.path);
+  imageElement.setAttribute('alt', chosenImage.alt);
+  imageElement.setAttribute('title', chosenImage.title);
+  parentElement.appendChild(imageElement);
+}
 
+function getRandomNumber(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
+function handleClick(event){
+  console.log('an image was clicked');
+  console.log('yes yes it works', event.target.alt);
+  var alt = event.target.alt;
+
+  for(var i=0; i<picArray.length; i++){
+    if(alt === picArray[i].alt){
+      picArray[i].clicks++;
+    }
+  }
+
+  parentElement.innerHTML = '';
+  getRandomImage();
+  getRandomImage();
+}
+
+parentElement.addEventListener('click', handleClick);
+
+getRandomImage();
+getRandomImage();
---
+++ user_stories.md/user_stories.md
@@ -0,0 +1,11 @@
+# User stories
+
+<!-- user stories are explaining what the functions we are righting are doing, and the actually idea of the website  -->
+
+* User 1
+
+1. I would like to see  a web site to help useres find things the would be intrested in buying by making a web page that cycles threw images of things and and if you liked that image you could maybe do a search for it and hopefully buy it.
+
+* User 2
+
+2. i would like to se 
